d2848bd64b84a0f893e91ff5d5587dd7f9d58606
ata <ataturkoglu@outlook.com.tr>
2021-11-18 23:12:18 +0300

    pagination

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -2,10 +2,15 @@ const Photo = require('../models/Photo');
 const fs = require('fs');
 
 exports.getAllPhotos = async (req, res) => {
-  //res.sendFile(path.resolve(__dirname, 'temp/index.html'));
-  const photos = await Photo.find().sort('-date');
+
+  const page =req.query.page || 1
+  const photosPerPage = 2
+  const totalPhotos = await Photo.find().countDocuments()
+  const photos = await Photo.find().sort('-date').skip((page-1)*photosPerPage).limit(photosPerPage)
   res.render('index', {
     photos,
+    current:page,
+    pages:Math.ceil(totalPhotos/photosPerPage)
   });
 };
 
